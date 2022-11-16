415304ac3b0c7af458edaa0c31deb4eb895b20e5
MisterMew <thebesteccentric@gmail.com>
2022-11-16 21:31:14 +1100

    v0.1.1 (pre-release)
    + Added in project section

--- Portfolio/content/index.js
+++ Portfolio/content/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var tabLinks = document.getElementsByClassName("aboutme-tab-links");
     var tabContents = document.getElementsByClassName("aboutme-tab-contents");
     
-    ///Open Tab
+    ///Open Tab///
     function OpenTab(tabname, contentname) {
         //De-activate all other classes
         for (tabLink of tabLinks) {
@@ -25,4 +25,21 @@ $(document).ready(function(){
         let clickedTab = $(this).attr("id");                  //Get the current id of the clicked tab 
         OpenTab(`#${clickedTab}`, `#${clickedTab}-content`); //Use the id to Open the desired tab
     })
+
+
+
+    ///Smooth Scrolling ///
+    $("a").on('click', function(event) {
+        if (this.hash !== "") //Make sure this.hash has a value before overriding default behavior
+        {
+            event.preventDefault(); //Prevent default anchor click behavior
+
+            var hash = this.hash; //Store hash
+            $('html, body').animate({ //Using jQuery's animate() method to add smooth page scroll
+                scrollTop: $(hash).offset().top
+            }, 800, function(){               //The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                window.location.hash = hash; //Add hash (#) to URL when done scrolling (default click behavior)
+            });
+        }
+    });
 })
