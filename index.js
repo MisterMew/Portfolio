4cfdb1ae1347489e0059aef1b0a9ecf9de53c495
MisterMew <thebesteccentric@gmail.com>
2022-12-07 13:25:45 +1100

    v1.0.8 - Fixing Media Queries

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     /* ABOUT ME: TABS */
     ///On Function Click
-    $(".aboutme-tab-links").click(function() {
+    $(".aboutme-tab-links").click(function () {
         let clickedTab = $(this).attr("id");                  //Get the current id of the clicked tab 
         OpenTab(`#${clickedTab}`, `#${clickedTab}-content`); //Use the id to Open the desired tab
     })
@@ -20,27 +20,27 @@ $(document).ready(function(){
         for (tabContent of tabContents) {
             $(tabContent).removeClass("active-tab");
         }
-        
+
         //Activate the classes
         $(contentname).addClass("active-tab");
         $(tabname).addClass("active-link");
     }
 
 
-    
+
     /* RETROSPECTIVE GALLERIES */
-        var focusedImageBox = document.getElementById("focusedImageBox");
-        var focusedImage = document.getElementById("focusedImage");
+    var focusedImageBox = document.getElementById("focusedImageBox");
+    var focusedImage = document.getElementById("focusedImage");
 
     var modal = $(focusedImageBox);
-    $(".openFocusedImage").click(function() {
+    $(".openFocusedImage").click(function () {
         let clickedImg = this.src;
 
         modal.css("display", "block");
         $(focusedImage.setAttribute("src", clickedImg));
     });
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (modal.css("display") != "none" && event.target == focusedImage) {
             modal.css("display", "none");
         }
@@ -56,7 +56,7 @@ $(document).ready(function(){
 
 
     ///Smooth Scrolling ///
-    $("a").on('click', function(event) {
+    $("a").on('click', function (event) {
         if (this.hash !== "") //Make sure this.hash has a value before overriding default behavior
         {
             event.preventDefault(); //Prevent default anchor click behavior
@@ -64,9 +64,21 @@ $(document).ready(function(){
             var hash = this.hash; //Store hash
             $('html, body').animate({ //Using jQuery's animate() method to add smooth page scroll
                 scrollTop: $(hash).offset().top
-            }, 800, function(){               //The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            }, 800, function () {               //The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 window.location.hash = hash; //Add hash (#) to URL when done scrolling (default click behavior)
             });
+        }
+    });
+
+
+    ///DISPLAY NAVIGATION///
+    $('nav-dropdown').click(function (e) {
+        e.preventDefault();
+
+        if ($('nav-items').hasClass('.show')) {
+            $('nav-items').removeClass('.show');
+        } else {
+            $('nav-items').addClass('.show');
         }
     });
 })
